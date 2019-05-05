5c6f5e458f0ba49388bbc38d7a1353851e359e4f
a <ruan65@gmail.com>
2019-05-05 18:26:45 +0300

    clean_clock

--- lib/clock/clock_hands.dart
+++ lib/clock/clock_hands.dart
@@ -1,17 +1,12 @@
-import 'dart:async';
-
 import 'package:clock_ui_app/clock/hand_hour.dart';
 import 'package:clock_ui_app/clock/hand_minute.dart';
 import 'package:clock_ui_app/clock/hand_second.dart';
 import 'package:flutter/material.dart';
 
-
-
 class ClockHands extends StatelessWidget {
   final DateTime dateTime;
-  final bool showHourHandleHeartShape;
 
-  ClockHands({this.dateTime, this.showHourHandleHeartShape = false});
+  ClockHands({this.dateTime});
 
   @override
   Widget build(BuildContext context) {
@@ -20,22 +15,18 @@ class ClockHands extends StatelessWidget {
         child: Container(
             width: double.infinity,
             padding: const EdgeInsets.all(20.0),
-            child: Stack(
-                fit: StackFit.expand,
-                children: <Widget>[
-                  CustomPaint( painter: HourHandPainter(
-                      hours: dateTime.hour, minutes: dateTime.minute, showHeartShape: showHourHandleHeartShape),
-                  ),
-                  CustomPaint(painter: MinuteHandPainter(
-                      minutes: dateTime.minute, seconds: dateTime.second),
-                  ),
-                  CustomPaint(painter: SecondHandPainter(seconds: dateTime.second),
-                  ),
-                ]
-            )
-        )
-
-    );
+            child: Stack(fit: StackFit.expand, children: <Widget>[
+              CustomPaint(
+                painter: HourHandPainter(
+                    hours: dateTime.hour, minutes: dateTime.minute),
+              ),
+              CustomPaint(
+                painter: MinuteHandPainter(
+                    minutes: dateTime.minute, seconds: dateTime.second),
+              ),
+              CustomPaint(
+                painter: SecondHandPainter(seconds: dateTime.second),
+              ),
+            ])));
   }
 }
-
