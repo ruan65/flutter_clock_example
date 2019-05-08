83f1d4fa4437aa82887a75361a2d16547ace5d0f
a <ruan65@gmail.com>
2019-05-08 22:46:03 +0300

    break

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SecondSreen extends StatelessWidget {
-  final double _smallFontSize = 12;
-  final double _valFontSize = 30;
+  final double _smallFontSize = 11;
+  final double _valFontSize = 25;
   final FontWeight _smallFontWeight = FontWeight.w500;
   final FontWeight _valFontWeight = FontWeight.w700;
   final Color _fontColor = Color(0xff5b6990);
@@ -51,39 +51,157 @@ class SecondSreen extends StatelessWidget {
                       )),
                 ],
               ),
+              Container(
+                height: 170,
+                width: 170,
+                decoration: BoxDecoration(
+                  color: Color(0xfff0f5fb),
+                  border: Border.all(
+                    width: 8,
+                    color: Color(0xffd3e1ed),
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'THIS WEEK',
+                      style: TextStyle(
+                          color: _fontColor,
+                          fontSize: _smallFontSize,
+                          fontWeight: _smallFontWeight,
+                          letterSpacing: _smallFontSpacing),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      height: 120,
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      width: double.infinity,
+                      child: CustomPaint(
+                        foregroundPainter: GraphPainter(),
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
           SizedBox(
             height: 25,
           ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-                border: Border(
-                    bottom: BorderSide(width: 1.5, color: Color(0xffdde9f7)))),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'THURSDAY',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: _fontColor,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Row(children: <Widget>[
-                  
-                ],)
+          RecrordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: 'SUNDAY'),
+          RecrordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: 'MONDAY'),
+          RecrordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: 'TUESDAY'),
+          RecrordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: 'WEDNESDAY'),
+          RecrordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: 'THURSDAY'),
+          RecrordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: 'FRIDAY'),
+          RecrordItem(
+              fontColor: _fontColor,
+              smallFontSpacing: _smallFontSpacing,
+              day: 'SUTURDAY'),
+        ],
+      ),
+    );
+  }
+}
 
-              ],
+class RecrordItem extends StatelessWidget {
+  final Color _fontColor;
+  final String day;
+  final double _smallFontSpacing;
+
+  const RecrordItem({
+    Key key,
+    @required Color fontColor,
+    @required String this.day,
+    @required double smallFontSpacing,
+  })  : _fontColor = fontColor,
+        _smallFontSpacing = smallFontSpacing,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+          border:
+              Border(bottom: BorderSide(width: 1.5, color: Color(0xffdde9f7)))),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            day,
+            style: TextStyle(
+              fontSize: 16,
+              color: _fontColor,
+              fontWeight: FontWeight.w700,
             ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                '01/21/2019',
+                style: TextStyle(
+                    fontSize: 13,
+                    letterSpacing: _smallFontSpacing,
+                    color: _fontColor),
+              ),
+              SizedBox(
+                width: 25,
+              ),
+              Expanded(
+                child: Text(
+                  '45.3 MINUTES',
+                  style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: _smallFontSpacing,
+                      color: _fontColor),
+                ),
+              ),
+            ],
           )
         ],
       ),
     );
   }
+}
+
+class GraphPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return null;
+  }
+
 }
