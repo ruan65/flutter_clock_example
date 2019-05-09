8caa04d9627b4b11a8596041f21be0c8ee479ccd
a <ruan65@gmail.com>
2019-05-09 17:31:23 +0300

    one bug left

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -52,12 +52,12 @@ class SecondSreen extends StatelessWidget {
                 ],
               ),
               Container(
-                height: 170,
+                height: 200,
                 width: 170,
                 decoration: BoxDecoration(
                   color: Color(0xfff0f5fb),
                   border: Border.all(
-                    width: 8,
+                    width: 5,
                     color: Color(0xffd3e1ed),
                   ),
                   borderRadius: BorderRadius.circular(15),
@@ -78,9 +78,9 @@ class SecondSreen extends StatelessWidget {
                       height: 15,
                     ),
                     Container(
-                      height: 120,
-                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      height: 150,
                       width: double.infinity,
+                      padding: EdgeInsets.symmetric(horizontal: 10),
                       child: CustomPaint(
                         foregroundPainter: GraphPainter(),
                       ),
@@ -193,15 +193,46 @@ class RecrordItem extends StatelessWidget {
 }
 
 class GraphPainter extends CustomPainter {
+  Paint trackBarPaint = Paint()
+    ..color = Color(0xff818aab)
+    ..style = PaintingStyle.stroke
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 12;
+
+  Paint trackPaint = Paint()
+    ..color = Color(0xffdee6f1)
+    ..style = PaintingStyle.stroke
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 12;
+
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
+    Path trackPath = Path();
+    Path trackBarPath = Path();
+
+    List val = [
+      size.height * 0.8,
+      size.height * 0.5,
+      size.height * 0.9,
+      size.height * 0.3,
+      size.height * 0.7,
+    ];
+
+    double origin = 8;
+
+    for (int i = 0; i < val.length; i++) {
+      trackPath.moveTo(origin, size.height);
+      trackPath.lineTo(origin, 0);
+      canvas.drawPath(trackPath, trackPaint);
+
+      trackBarPath.moveTo(origin, size.height);
+      trackBarPath.lineTo(origin, val[i]);
+      canvas.drawPath(trackBarPath, trackBarPaint);
+
+      origin = origin + size.width * 0.22;
+    }
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    return null;
-  }
-
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
